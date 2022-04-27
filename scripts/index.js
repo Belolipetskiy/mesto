4c476bfca01221910a892b065ebd68a95db697a4
Belolipetskiy <96343246+Belolipetskiy@users.noreply.github.com>
2022-04-27 17:45:53 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,152 @@
-let editProfileBtn = document.querySelector('.profile__edit-button');
-let modalWindow = document.querySelector('.popup');
-let modalCloseBtn = modalWindow.querySelector('.popup__close');
-let editNameInfo = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_text_name');
-let aboutInput = document.querySelector('.popup__input_text_about');
-let profileName = document.querySelector('.profile__info-title');
-let profileAbout = document.querySelector('.profile__info-subtitle');
+const popupProfile = document.querySelector('.popup_profile');
+const editProfileBtn = document.querySelector('.profile__edit-button');
+const editNameInfo = popupProfile.querySelector('.popup__form-profile');
+const closePopupProfile = popupProfile.querySelector('.popup__close');
+const nameInputProfile = popupProfile.querySelector('.popup__input_profile_name');
+const aboutInputProfile = popupProfile.querySelector('.popup__input_profile_about');
+const profileName = document.querySelector('.profile__info-title');
+const profileAbout = document.querySelector('.profile__info-subtitle');
 
-//функция открытия popup
-function openModalWindow() {
-    modalWindow.classList.add('popup_opened'); {
+const popupAddCard = document.querySelector('.popup_add-card');
+const addCardBtn = document.querySelector('.profile__add-card');
+const editNameCard = popupAddCard.querySelector('.popup__form-card');
+const closePopupCard = popupAddCard.querySelector('.popup__close');
+const listContainer = document.querySelector('.element-grid');
+const cardTemplate = document.querySelector('.card-template');
+const nameInputCard = document.querySelector('.popup__input_card_name');
+const linkInputCard = document.querySelector('.popup__input_card_link');
 
-        nameInput.value = profileName.textContent;
-        aboutInput.value = profileAbout.textContent;
-    }
+const popupOpenCard = document.querySelector('.popup_open-card'); 
+const bigCard = popupOpenCard.querySelector('.popup__image'); 
+const bigCardTitle = popupOpenCard.querySelector('.popup__image-title'); 
+const closePopupBigCard = popupOpenCard.querySelector('.popup__close');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function openModalWindow(popup) {
+    popup.classList.add('popup_opened'); {}
 }
 
-function closeModalWindow() {
-    modalWindow.classList.remove('popup_opened'); {
-
-    }
+function closeModalWindow(popup) {
+    popup.classList.remove('popup_opened'); {}
 }
 
-function saveProfile(evt) {
+addCardBtn.addEventListener('click', () => {
+    openModalWindow(popupAddCard);
+});
+
+function render() {
+    const drawingElements = initialCards.map(getElement);
+    listContainer.append(...drawingElements);
+}
+
+function getElement(item) {
+    const getElemenTemplate = cardTemplate.content.cloneNode(true);
+    const link = getElemenTemplate.querySelector('.card__img');
+    const name = getElemenTemplate.querySelector('.card__title');
+    const removeButton = getElemenTemplate.querySelector('.card__bin');
+    const likeCardBtn = getElemenTemplate.querySelector('.card__like');
+
+    link.src = item.link;
+    link.alt = item.name;
+    name.textContent = item.name;
+
+    removeButton.addEventListener('click', removeCard);
+
+    likeCardBtn.addEventListener('click', function () {
+        likeCardBtn.classList.toggle('card__like_active')
+    });
+
+    link.addEventListener('click', function () {
+
+        bigCard.src = link.src;
+        bigCard.alt = link.alt;
+        bigCardTitle.textContent = name.textContent;
+
+        openModalWindow(popupOpenCard);
+    });
+
+    return getElemenTemplate;
+}
+
+
+function addCard(evt) {
+
+    const cardElement = getElement({
+        name: nameInputCard.value,
+        link: linkInputCard.value
+    });
+
+    listContainer.prepend(cardElement);
+
+    nameInputCard.value = '';
+    linkInputCard.value = '';
+}
+
+editNameCard.addEventListener('submit', evt => {
     evt.preventDefault();
 
-    profileName.textContent = nameInput.value;
-    profileAbout.textContent = aboutInput.value;
+    closeModalWindow(popupAddCard);
 
-    closeModalWindow();
+    addCard();
+});
+
+closePopupCard.addEventListener('click', () => {
+    closeModalWindow(popupAddCard);
+});
+
+function removeCard(evt) {
+    const cardElement = evt.target.closest('.card');
+    cardElement.remove();
 }
 
-editNameInfo.addEventListener('submit', saveProfile);
+editProfileBtn.addEventListener('click', () => {
+    nameInputProfile.value = `${profileName.textContent}`;
+    aboutInputProfile.value = `${profileAbout.textContent}`;
 
-editProfileBtn.addEventListener('click', openModalWindow);
+    openModalWindow(popupProfile);
+});
 
-modalCloseBtn.addEventListener('click', closeModalWindow);
+closePopupProfile.addEventListener('click', () => {
+    closeModalWindow(popupProfile);
+});
+
+editNameInfo.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    profileName.textContent = nameInputProfile.value;
+    profileAbout.textContent = aboutInputProfile.value;
+
+    closeModalWindow(popupProfile);
+});
+
+closePopupBigCard.addEventListener('click', () => {
+    closeModalWindow(popupOpenCard);
+});
+
+render();
